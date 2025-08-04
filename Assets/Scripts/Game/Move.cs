bf755b48ca15b1e79148f209922758f409c1d985
Seven1shi7 <2094779712@qq.com>
2025-08-04 20:24:29 +0800

    111

--- Assets/Scripts/Game/Move.cs
+++ Assets/Scripts/Game/Move.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,14 +10,34 @@ public class Move : MonoBehaviour
     public Yaogan yaogan;
     private bool isdown = false;
     public Animation animation;
+    public Button accack1;
+    public Button accack2;
+    public Button accack3;
+    public Button accack4;
     // Start is called before the first frame update
     void Start()
     {
         yaogan.move = move;
         animation = GetComponent<Animation>();
+        accack1.onClick.AddListener(() => {
+
+            animation.Play("attack");
+        });
+
+        accack2.onClick.AddListener(() => {
+            animation.Play("attack2");
+        });
+
+        accack3.onClick.AddListener(() => {
+            animation.Play("attack3");
+        });
+
+        accack4.onClick.AddListener(() => {
+            animation.Play("skill");
+        });
 
 
-       
+
     }
 
     private void move(Vector2 vector)
@@ -28,23 +49,26 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+     
         if (yaogan.ismove)
         {
             // isdown = true;
             transform.position += transform.forward * 5 * Time.deltaTime;
             animation.Play("walk");
-            
         }
         else
         {
             animation.Stop("walk");
-            animation.Play("idle");
+            //animation.Play("idle");
         }
-
-      
 
     }
 
+
+    public void LateUpdate()
+    {
+        
+
+    }
 
 }
